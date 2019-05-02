2d971d01ec9dbcd1e3e40458408199cb0a138839
Edgar Damasceno <edgar.damasceno@hotmail.com>
2019-05-01 23:44:49 -0300

    File and folder validation Restoring my Tilix settings Changes in the README Added a screenshot

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# my-tilix
+# My Tilix
 Installation, configuration and customization of Tilix for my development environment.
+
+## Appearance
+![My Tilix Look](/images/tilix.png)
+
+## Dependency
+* Ubuntu
+* bash
+* git
+* curl
+
+**To execute this script run the command:**
+```bash
+curl -s https://raw.githubusercontent.com/edgardamasceno/my-tilix/master/configure.sh | bash -s
+```
+
--- configure.sh
+++ configure.sh
@@ -4,8 +4,12 @@
 sudo add-apt-repository ppa:webupd8team/terminix -y
 sudo apt install tilix
 
-# VTE Configuration Fix
-ln -s /etc/profile.d/vte-2.91.sh /etc/profile.d/vte.sh
+# VTE configuration fix
+FILE=/etc/profile.d/vte.sh
+if [ ! -f "$FILE" ]; then
+    ln -s /etc/profile.d/vte-2.91.sh /etc/profile.d/vte.sh
+fi
+
 write_rc () {
    echo -e "\n# VTE Configuration Fix for Tilix\nif [ \$TILIX_ID ] || [ \$VTE_VERSION ]; then\n\tsource /etc/profile.d/vte.sh\nfi" >> ~/$1
 }
@@ -13,7 +17,16 @@ write_rc () {
 write_rc .bashrc
 write_rc .zshrc
 
-# Tilix Themes
-mkdir -p ~/.config/tilix/schemes/
-git clone https://github.com/storm119/Tilix-Themes.git /tmp/tilix-themes
-mv /tmp/tilix-themes/Themes/* ~/.config/tilix/schemes/
+# Tilix themes
+DIR=/etc/profile.d/vte.sh
+if [ ! -d "$DIR" ]; then
+    mkdir -p ~/.config/tilix/schemes/
+    THEMES=/tmp/tilix-themes
+   if [ ! -d "$THEMES" ]; then
+      git clone https://github.com/storm119/Tilix-Themes.git /tmp/tilix-themes
+      mv /tmp/tilix-themes/Themes/* ~/.config/tilix/schemes/   
+   fi
+fi
+
+# Restore my personal configuration
+curl -s https://raw.githubusercontent.com/edgardamasceno/my-tilix/master/tilix.dconf | dconf load /com/gexperts/Tilix/
